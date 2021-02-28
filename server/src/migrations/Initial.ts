ea2ea4850663e941c158ad32eb994fd3b444c48c
Jan Willem van Jaarsveld <janwvjaarsveld@gmail.com>
2021-02-28 17:18:36 +0200

    add dummy data

--- server/src/migrations/Initial.ts
+++ server/src/migrations/Initial.ts
@@ -22,6 +22,45 @@ export class Initial1614523509468 implements MigrationInterface {
         await queryRunner.query(
             `ALTER TABLE "post" ADD CONSTRAINT "FK_9e91e6a24261b66f53971d3f96b" FOREIGN KEY ("creatorId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
         );
+
+        // add admin user
+        await queryRunner.query(
+            `insert into public."user" (name, username, email, "password", "createdAt", "updatedAt", "isAdmin") values ('Administrator', 'admin', 'admin@xfour.co.za', '$argon2i$v=19$m=4096,t=3,p=1$33ivPANOIhPJ/66XvNSYfQ$sA76AU9GqK2xhIOdW2/vZ2L0xOOwAxnKa5by7Gt4S2c', now(), now(), true);`
+        );
+
+        // add data
+        await queryRunner.query(
+            `insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Analyst Programmer', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 1, '2020-02-03T02:35:54Z', '2019-08-18T03:55:59Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Nuclear Power Engineer', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 1, '2020-09-11T17:48:05Z', '2019-06-16T19:32:32Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Operator', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+            
+            Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 1, '2019-09-22T15:51:11Z', '2019-03-21T08:56:57Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Chief Design Engineer', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+            
+            Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 1, '2019-11-11T14:50:48Z', '2020-09-18T16:29:03Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Structural Analysis Engineer', 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+            
+            Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', 1, '2019-08-10T03:35:26Z', '2020-04-29T20:22:41Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Human Resources Assistant II', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+            
+            Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
+            
+            Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 1, '2019-07-26T16:08:07Z', '2019-12-01T16:26:55Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Automation Specialist III', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', 1, '2021-01-13T13:38:38Z', '2019-07-27T18:11:01Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Payment Adjustment Coordinator', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+            
+            Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+            
+            Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 1, '2019-11-12T05:50:53Z', '2020-02-26T05:03:40Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Environmental Specialist', 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', 1, '2019-03-24T01:44:51Z', '2019-10-05T17:10:30Z');
+            insert into post (title, text, "creatorId", "createdAt", "updatedAt") values ('Help Desk Operator', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
+            
+            Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+            
+            Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', 1, '2019-08-21T22:33:20Z', '2019-09-04T07:41:29Z');
+            
+            `
+        );
     }
 
     public async down(_: QueryRunner): Promise<void> {
